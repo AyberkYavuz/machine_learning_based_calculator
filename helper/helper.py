1f22a71efe92baadad604b022d801bd8b733f2c2
hikmetyavuz <hikmet.yavuz@misli.com>
2021-07-09 15:23:20 +0300

    RegressionHelper class was written.

--- helper/helper.py
+++ helper/helper.py
@@ -1,5 +1,6 @@
 import random
 import string
+from sklearn.metrics import mean_squared_error, r2_score
 
 
 class BackendHelper:
@@ -8,4 +9,15 @@ class BackendHelper:
         token = ''.join(random.choice(string.ascii_uppercase + string.ascii_lowercase + string.digits) for _ in range(18))
         return token
 
+
+class RegressionHelper:
+    @staticmethod
+    def show_regression_model_metrics(regression_model, y_test, y_test_prediction):
+        print('Coefficients: \n', regression_model.coef_)
+
+        print('Mean squared error: %.2f'
+              % mean_squared_error(y_test, y_test_prediction))
+
+        print('Coefficient of determination: %.2f'
+              % r2_score(y_test, y_test_prediction))
 
