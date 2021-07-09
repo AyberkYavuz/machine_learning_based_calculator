c2edeb3cf168f9fac6fd8f00ad722f0382e312db
hikmetyavuz <hikmet.yavuz@misli.com>
2021-07-09 17:58:02 +0300

    SummationMachineLearningModelAPI class was written.

--- web_application_backend.py
+++ web_application_backend.py
@@ -1,6 +1,12 @@
-from flask import Flask, render_template
+from flask import Flask
+from flask import render_template
+from flask import request
+from flask import jsonify
 from flask.views import MethodView
 from helpers.application_helpers import BackendHelper
+import pandas as pd
+from file_operations.file_handlers import PickleHandler
+import os
 
 app = Flask(__name__)
 app.secret_key = BackendHelper.create_random_aplhanumeric_string()
@@ -11,6 +17,47 @@ class MainPageSenderAPI(MethodView):
         return render_template("main_page.html")
 
 
+class SummationMachineLearningModelAPI(MethodView):
+
+    def get(self):
+        return "Welcome to summation machine learning model API!", 200
+
+    def post(self):
+        print("SummationMachineLearningModelAPI POST Method")
+        msg = None
+
+        cond1 = "number1" not in request.form
+        cond2 = "number2" not in request.form
+
+        if cond1 or cond2:
+            msg = "422", 422
+        else:
+            try:
+                print("try")
+                number1 = request.form['number1']
+                number2 = request.form['number2']
+                features_data = [[number1, number2]]
+                x_test = pd.DataFrame(features_data, columns=['Number1', 'Number2'])
+                print("loading summation machine learning model")
+                working_directory = os.getcwd()
+                summation_model_path = working_directory + '/machine_learning_models/' \
+                                                           'summation_machine_learning_model.pickle'
+                pickle_handler = PickleHandler()
+                summation_machine_learning_model = pickle_handler.load_object(summation_model_path)
+                print("making prediction")
+                prediction = summation_machine_learning_model.predict(x_test)[0]
+                prediction = round(prediction, 2)
+                print(prediction)
+                msg = jsonify({"prediction": prediction}), 200
+            except Exception as ex:
+                print("Exception : " + str(ex))
+                msg = "Something went wrong!", 500
+
+        return msg
+
+
 main_page_sender_view = MainPageSenderAPI.as_view('main_page_sender_api')
 app.add_url_rule('/', view_func=main_page_sender_view, methods=['GET'])
+summation_machine_learning_model_view = SummationMachineLearningModelAPI.as_view('summation_machine_learning_model_api')
+app.add_url_rule('/summation', view_func=summation_machine_learning_model_view, methods=['POST', 'GET'])
 app.run(debug=True)
