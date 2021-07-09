059914ab687dfc29f4f6d5b199a30eec248d9dd2
hikmetyavuz <hikmet.yavuz@misli.com>
2021-07-09 17:13:19 +0300

    server_request_handler.js was written.

---
+++ static/js/server_request_handler.js
@@ -0,0 +1,24 @@
+$("#add_button").click(function(e) {
+    e.preventDefault(); // prevents page refreshing
+    var first_number_value = $("#first_number").val();
+    var second_number_value = $("#second_number").val();
+    var condition1 = first_number_value == "";
+    var condition2 = second_number_value == "";
+    if (condition1 || condition2) {
+        console.log("Empty values")
+    } else {
+        console.log("Values are not empty!");
+        $.ajax({
+              type : 'POST',
+              url : "/summation",
+              data : {'number1':first_number_value, 'number2': second_number_value},
+              success: function(response) {
+                console.log(response);
+              },
+              error: function(error) {
+                console.log(error);
+              }
+        });
+    }
+
+});
