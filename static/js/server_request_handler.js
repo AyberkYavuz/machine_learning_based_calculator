64f8f19597dc8c5434b19eb601d7dd3ee72819cc
hikmetyavuz <hikmet.yavuz@misli.com>
2021-07-09 19:06:27 +0300

    server_request_handler.js was updated.

--- static/js/server_request_handler.js
+++ static/js/server_request_handler.js
@@ -1,7 +1,8 @@
-$("#add_button").click(function(e) {
-    e.preventDefault(); // prevents page refreshing
-    var first_number_value = $("#first_number").val();
-    var second_number_value = $("#second_number").val();
+function capitalizeFirstLetter(string) {
+  return string.charAt(0).toUpperCase() + string.slice(1);
+}
+
+function request_server(first_number_value, second_number_value, url){
     var condition1 = first_number_value == "";
     var condition2 = second_number_value == "";
     if (condition1 || condition2) {
@@ -10,11 +11,13 @@ $("#add_button").click(function(e) {
         console.log("Values are not empty!");
         $.ajax({
               type : 'POST',
-              url : "/summation",
+              url : url,
               data : {'number1':first_number_value, 'number2': second_number_value},
               success: function(response) {
                 console.log(response);
-                Materialize.toast('Summation Prediction: ' + response['prediction'].toString(), 5000)
+                var operation_name = url.substring(1);
+                operation_name = capitalizeFirstLetter(operation_name);
+                Materialize.toast(operation_name + ' Prediction: ' + response['prediction'].toString(), 5000)
               },
               error: function(error) {
                 console.log(error);
@@ -22,30 +25,20 @@ $("#add_button").click(function(e) {
         });
     }
 
+}
+
+$("#add_button").click(function(e) {
+    e.preventDefault(); // prevents page refreshing
+    var first_number_value = $("#first_number").val();
+    var second_number_value = $("#second_number").val();
+    url = "/summation";
+    request_server(first_number_value, second_number_value, url);
 });
 
 $("#substruction_button").click(function(e) {
     e.preventDefault(); // prevents page refreshing
     var first_number_value = $("#first_number_subtraction").val();
     var second_number_value = $("#second_number_subtraction").val();
-    var condition1 = first_number_value == "";
-    var condition2 = second_number_value == "";
-    if (condition1 || condition2) {
-        console.log("Empty values")
-    } else {
-        console.log("Values are not empty!");
-        $.ajax({
-              type : 'POST',
-              url : "/substruction",
-              data : {'number1':first_number_value, 'number2': second_number_value},
-              success: function(response) {
-                console.log(response);
-                Materialize.toast('Subtraction Prediction: ' + response['prediction'].toString(), 5000)
-              },
-              error: function(error) {
-                console.log(error);
-              }
-        });
-    }
-
+    var url = "/substruction";
+    request_server(first_number_value, second_number_value, url);
 });
